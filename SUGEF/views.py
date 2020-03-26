756907efe246cd0262efd417d57c6644a350199b
Eribaldo <eribaldo.barros@escolar.ifrn.edu.br>
2020-03-26 17:15:09 -0300

    Eribaldo, terminando de adicionar primeiros arquivos

--- SUGEF/views.py
+++ SUGEF/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import *
 
-# Create your views here.
+hora = timezone.now().hour
+
+def login(request):
+      return render(request, 'login.html', {})
